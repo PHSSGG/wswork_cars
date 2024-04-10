cd3e84b5f96ad89e0d691a67560a56be92ade271
PHSSGG <pedrohddgd@gmail.com>
2024-04-10 10:59:31 -0300

    added cors config

---
+++ src/main/kotlin/phss/wsworkcars/config/CorsConfig.kt
@@ -0,0 +1,17 @@
+package phss.wsworkcars.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+
+}
